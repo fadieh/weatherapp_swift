bdbc5bee9f04d3212260687e0a69730006994bfa
Fadie Hannona <fadiehannona@gmail.com>
2015-02-10 20:40:26 +0000

    bit of refactoring and creating basic algorithm

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -19,6 +19,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var humidText: UILabel!
     @IBOutlet weak var icons: UIImageView!
     @IBOutlet weak var weatherSummary: UILabel!
+    
+    var tempScore = 0
+    var currentTemp = 0
+    var windSpeed = 0
+    var windSpeedEffectOnTemperature = 0
+    var humidityPercent = 0
+    var humidityEffectOnTemperature = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,33 +56,56 @@ class ViewController: UIViewController {
                 var weatherSum = jsonObj["weather"][0]["description"].stringValue
                 var windSum = jsonObj["wind"]["speed"].intValue
                 var humidity = jsonObj["main"]["humidity"].intValue
-                println(windSum)
-                self.setData(location, celTemp: celTemp, weather: weather, weatherSum: weatherSum, windSum: windSum, humidity: humidity)
+                self.currentTemp = jsonObj["main"]["temp"].intValue - 273
+                self.windSpeed = jsonObj["wind"]["speed"].intValue
+                self.humidityPercent = jsonObj["main"]["humidity"].intValue
+                self.setData(location, weatherSum: weatherSum)
             }
         }
-        
-        
     }
     
-    func setData(location: String, celTemp: Int, weather: String, weatherSum: String, windSum: Int, humidity: Int) {
-        self.cityText.text = location.uppercaseString
-        self.celTemp.text = String(celTemp) + "°C"
-        self.weatherSummary.text = "(" + weatherSum.capitalizedString + ")"
-        self.windText.text = "Wind speed: " + String(windSum) + " mph"
-        self.humidText.text = String(humidity) + "% humidity"
-        println(NSDate())
-        if (weather == "Clouds" && celTemp < 10 && celTemp > 4) {
-            self.weatherText.text = "At least triple layer it today."
-        } else if (weather == "Clouds" && celTemp < 4 && celTemp > 0) {
-            self.weatherText.text = "I'd wear some gloves if I were you..."
-        } else if (weather == "Clouds" && celTemp <= 0) {
-            self.weatherText.text = "Throw yourself in a fire, it's below freezing!"
-        }
+    func setData(location: String, weatherSum: String) {
+        self.setText(location, weatherSum: weatherSum)
+        determineTempScore()
+        
+//        if (weather == "Clouds" && celTemp < 10 && celTemp > 4) {
+//            self.weatherText.text = "At least triple layer it today."
+//        } else if (weather == "Clouds" && celTemp < 4 && celTemp > 0) {
+//            self.weatherText.text = "I'd wear some gloves if I were you..."
+//        } else if (weather == "Clouds" && celTemp <= 0) {
+//            self.weatherText.text = "Throw yourself in a fire, it's below freezing!"
+//        }
         
         if (location == "London") {
             self.icons.image = UIImage(named: "london-01.png")
         } else {
             self.icons.image = nil
+        }
+        
+    }
+    
+    func setText(location: String, weatherSum: String) {
+        self.cityText.text = location.uppercaseString
+        self.weatherSummary.text = "(" + weatherSum.capitalizedString + ")"
+        self.celTemp.text = String(self.currentTemp)
+        self.windText.text = String(self.windSpeed) + "mph winds"
+        self.humidText.text = String(self.humidityPercent) + "% humidity"
+    }
+    
+    func determineTempScore() {
+        
+        self.tempScore = self.currentTemp
+        
+        if (self.humidityPercent >= 50) {
+            self.tempScore += 2
+        } else {
+            self.tempScore -= 2
+        }
+        
+        if (self.windSpeed < 6) {
+            self.tempScore += 2
+        } else {
+            self.tempScore -= 2
         }
         
     }
